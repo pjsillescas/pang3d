7fd4a4d8bd84c0257cb29391332429eb5d440fd4
pjsillescas <pjsillescas@gmail.com>
2026-04-12 21:19:22 +0200

    Los jugadores pueden empezar otro continue al acabársele las vidas si el otro jugador sigue activo.

---
+++ pang3d/Assets/Scripts/GameOverWatcher.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class GameOverWatcher : MonoBehaviour
+{
+	private InputManager inputManager;
+	private GameManager gameManager;
+	private GameStats gameStats;
+	private PlayerChoices playerChoices;
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		gameManager = FindAnyObjectByType<GameManager>();
+		gameStats = FindAnyObjectByType<GameStats>();
+		playerChoices = FindAnyObjectByType<PlayerChoices>();
+
+		inputManager = FindAnyObjectByType<InputManager>();
+		inputManager.OnHook += OnStartNewContinue;
+	}
+
+	private void OnDestroy()
+	{
+		inputManager.OnHook -= OnStartNewContinue;
+	}
+
+	private bool PlayerCanStartNewGame(int playerId)
+	{
+		var stats1 = gameStats.GetPlayerData(1);
+		var stats2 = gameStats.GetPlayerData(2);
+
+		if (playerId == 1)
+		{
+			return playerChoices.GetPlayer2Prefab() != null && stats2.GetLives() > 0 && stats1.GetLives() == 0;
+		}
+		else
+		{
+			return playerChoices.GetPlayer1Prefab() != null && stats1.GetLives() > 0 && stats2.GetLives() == 0;
+		}
+	}
+
+	private void OnStartNewContinue(object sender, int playerId)
+	{
+		if(PlayerCanStartNewGame(playerId))
+		{
+			gameStats.GetPlayerData(playerId).Reset();
+			if (playerId == 1)
+			{
+				gameManager.SpawnPlayer1();
+			}
+			else
+			{
+				gameManager.SpawnPlayer2();
+			}
+		}
+	}
+}
